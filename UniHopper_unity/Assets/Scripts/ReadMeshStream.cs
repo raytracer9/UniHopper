084c2aabc094e3541b6e4bc195832054c08d6894
raytracer9 <46299181+raytracer9@users.noreply.github.com>
2019-01-03 00:44:05 +0100

    Texfile to Mesh Parser 01
    First atempt to Parse two text files containing vertices and triangles and convert them to a mesh in Unity.

--- UniHopper_unity/Assets/Scripts/ReadMeshStream.cs
+++ UniHopper_unity/Assets/Scripts/ReadMeshStream.cs
@@ -7,37 +7,57 @@ using UnityEngine;
 public class ReadMeshStream : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    string triPath = "M:/streamGH/triangle01.txt";
+    string vertPath = "M:/streamGH/vert01.txt";
+
     void Start()
     {
-        string triPath = "M:/streamGH/triangle01.txt";
-        string vertPath = "M:/streamGH/vert01.txt";
-
-        StreamReader vertReader = new StreamReader(vertPath);
-
-
-
-
-        foreach (string word in triData)
-        {
-            Debug.Log(word);
-
-        }
-    }
-
-    Vector3[] convertTriangleToV3Array(string triPath)
-    {
-        StreamReader triReader = new StreamReader(triPath);
-        string[] triData = triReader.ReadToEnd().Replace("T{", "").Replace("}", "").Replace("\r\n", "-").Split('-');
-
-        foreach (string item in triData)
-        {
-
-        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        gameObject.GetComponent<MeshFilter>().mesh = ConstructMesh(ConvertTriangleToInt(triPath), ConvertVertexToV3Array(vertPath));
     }
+
+    int[] ConvertTriangleToInt(string triPath)
+    {
+        StreamReader triReader = new StreamReader(triPath);
+        string[] triData = triReader.ReadToEnd().Replace("T{", "").Replace("}", "").Replace("\r\n", ";").Split(';');
+        triReader.Close();
+        int[] triInt = new int[triData.Length];
+        for (int i = 0; i < triData.Length; i++)
+        {
+            triInt[i] = int.Parse(triData[i].Split(';')[0]);
+        }
+        return triInt;
+    }
+
+    Vector3[] ConvertVertexToV3Array(string vertPath)
+    {
+        StreamReader vertReader = new StreamReader(vertPath);
+        string[] vertData = vertReader.ReadToEnd().Replace("{", "").Replace("}", "").Replace("\r\n", ";").Split(';');
+        vertReader.Close();
+        Debug.Log(vertData.Length);
+        Debug.Log(vertData[5]);
+        Vector3[] vertVec3 = new Vector3[vertData.Length];
+
+        for (int i = 0; i < vertData.Length; i++)
+        {
+            vertVec3[i] = new Vector3(float.Parse(vertData[i].Split(',')[0]),
+                float.Parse(vertData[i].Split(',')[1]),
+                float.Parse(vertData[i].Split(',')[2]));
+        }
+        return vertVec3;
+    }
+
+    Mesh ConstructMesh(int[] triangles, Vector3[] vertices)
+    {
+        Mesh genMesh = new Mesh();
+        genMesh.vertices = vertices;
+        genMesh.triangles = triangles;
+        return genMesh;
+    }
+
 }
