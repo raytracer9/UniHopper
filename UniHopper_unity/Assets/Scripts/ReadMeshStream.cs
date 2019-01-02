127788e728d0834bedc6648904046b024bbbcb13
raytracer9 <46299181+raytracer9@users.noreply.github.com>
2019-01-03 00:01:05 +0100

    initial dev commit

---
+++ UniHopper_unity/Assets/Scripts/ReadMeshStream.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadMeshStream : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string triPath = "M:/streamGH/triangle01.txt";
+        string vertPath = "M:/streamGH/vert01.txt";
+
+        StreamReader vertReader = new StreamReader(vertPath);
+
+
+
+
+        foreach (string word in triData)
+        {
+            Debug.Log(word);
+
+        }
+    }
+
+    Vector3[] convertTriangleToV3Array(string triPath)
+    {
+        StreamReader triReader = new StreamReader(triPath);
+        string[] triData = triReader.ReadToEnd().Replace("T{", "").Replace("}", "").Replace("\r\n", "-").Split('-');
+
+        foreach (string item in triData)
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
