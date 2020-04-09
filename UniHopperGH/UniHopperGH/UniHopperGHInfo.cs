d0299388b461631ae8fea6ca816875b28dcda445
raytracer9 <46299181+raytracer9@users.noreply.github.com>
2020-04-09 14:35:53 +0200

    Neuanfang

---
+++ UniHopperGH/UniHopperGH/UniHopperGHInfo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper.Kernel;
+
+namespace UniHopperGH
+{
+    public class UniHopperGHInfo : GH_AssemblyInfo
+    {
+        public override string Name
+        {
+            get
+            {
+                return "UniHopperGH";
+            }
+        }
+        public override Bitmap Icon
+        {
+            get
+            {
+                //Return a 24x24 pixel bitmap to represent this GHA library.
+                return null;
+            }
+        }
+        public override string Description
+        {
+            get
+            {
+                //Return a short string describing the purpose of this GHA library.
+                return "";
+            }
+        }
+        public override Guid Id
+        {
+            get
+            {
+                return new Guid("11c7ef52-411d-4dae-96aa-4c89e016f38c");
+            }
+        }
+
+        public override string AuthorName
+        {
+            get
+            {
+                //Return a string identifying you or your company.
+                return "";
+            }
+        }
+        public override string AuthorContact
+        {
+            get
+            {
+                //Return a string representing your preferred contact details.
+                return "";
+            }
+        }
+    }
+}
